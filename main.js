fee1386ab80c2cf56f819e6eb303e427d85d10a4
Yannik <me@adnidor.de>
2017-01-17 22:38:40 +0100

    fixed console message

--- main.js
+++ main.js
@@ -61,5 +61,5 @@ var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Basicmathasaservice listening at http://%s:%s", host, port)
 })
